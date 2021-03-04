a8d1fb09bc17358449aadd8a2c66bd9043f311c8
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-04 11:29:42 -0300

    Listado de tarjetas

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
+  listTarjetas: any[] = [
+    { titular: 'Juan Perez', numeroTarjeta: '25252526262', fechaExpiracion: '11/23', cvv: '123' },
+    { titular: 'Miguel Gonzalez', numeroTarjeta: '252526262', fechaExpiracion: '11/24', cvv: '312' },
+  ];
 
   constructor() { }
 
