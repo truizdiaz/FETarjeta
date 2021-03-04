9cb427e779bb053ce673994f89ce17991efaaa84
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-04 11:01:44 -0300

    Confiugracion uigradients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaCreditoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrls: ['./tarjeta-credito.component.css']
+})
+export class TarjetaCreditoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
